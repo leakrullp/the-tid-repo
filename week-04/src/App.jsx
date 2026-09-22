2d2c0fa9bc3d056c4f211653da609f79bcd183a6
leakrullp <leakrull@proton.me>
2026-09-22 16:48:28 +0200

    Initialize Parse with application and JavaScript keys

--- week-04/src/App.jsx
+++ week-04/src/App.jsx
@@ -2,7 +2,10 @@ import "./App.css";
 import ToDoList from "./ToDoList.jsx";
 import Parse from "parse";
 
-Parse.initialize("YOUR_APP_ID", "YOUR_JAVASCRIPT_KEY");
+Parse.initialize(
+  "gIfWHFPdXe70K1Tvg0ma0892ovyFKbaKbgwAHjVU",
+  "rCQCXMFLdKSFNbfNuUaYYw1wceh6kZdtuTAAq6nI",
+);
 Parse.serverURL = "https://parseapi.back4app.com";
 
 function App() {
